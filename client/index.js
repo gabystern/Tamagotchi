4fa2dfacbf47273ee3ad5e6d3e8f3425c40ef2d9
github username <github email address>
2017-05-30 16:47:52 -0400

    finishing up form

--- client/index.js
+++ client/index.js
@@ -1,45 +1,49 @@
 
 $(document).ready(function() {
 	fetchToys()
+	fetchSettings()
 
+	// let image = ""
+	$(".img-check").click(function(){
+		$(this).toggleClass("check");
+		// image = this.src
+	})
+	
+	$('#new_pet').on('submit', function(event){
+		event.preventDefault()
+		let values = $(this).serialize()
+		console.log(image)
+		debugger
+	})
 })
 
-	function fetchToys(){
-		$.ajax({
-			url:'http://localhost:3000/api/v1/toys',
-			success: function(data){
-				let toyNames = data.map(function(toy){
-					let name = toy.name
-					let id = toy.id
-					let checkbox = `<input type="checkbox" id=${id}><label> ${name} </label> `
-					// debugger
-					$('.toys').append(checkbox)
-				})
-			}
-		})
-	}
-
-	function fetchPets(){
-		$.ajax({
-			url:'http://localhost:3000/api/v1/pets',
-			success: displayPets
-		})
-	}
-
-	function fetchsettings(){
-			$.ajax({
-				url:'http://localhost:3000/api/v1/settings',
-				success: displaysettings
+function fetchToys(){
+	$.ajax({
+		url:'http://localhost:3000/api/v1/toys',
+		success: function(data){
+			let toyNames = data.map(function(toy){
+				let name = toy.name
+				let id = toy.id
+				let checkbox = `<input type="checkbox" id=${id}><label> ${name} </label> `
+				// debugger
+				$('.toys').append(checkbox)
 			})
 		}
+	})
+}
 
+function fetchSettings(){
+	$.ajax({
+		url:'http://localhost:3000/api/v1/settings',
+		success: function(data){
+			let settings = data.map(function(setting){
+				let location = setting.location
+				let id = setting.id
+				let selectbox = `<option value=${location} id=${id}> ${location} </option> `
+				$('.settings select').append(selectbox)
+			})
+		}
+	})
+}
 
-
-	// const toys = function(data){
-	// 	$toys = $('#toys')
-	// 	$toys.html = ""
-	// 	data.forEach(function (toy){
-
-	// 	})
-	// }
 
