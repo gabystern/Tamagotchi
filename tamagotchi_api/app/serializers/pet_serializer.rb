5fea1f31560d0870f90c201af36c03851d713a20
github username <github email address>
2017-05-31 14:24:07 -0400

    fixed serializers started show-pet in showDetails

--- tamagotchi_api/app/serializers/pet_serializer.rb
+++ tamagotchi_api/app/serializers/pet_serializer.rb
@@ -1,5 +1,5 @@
-class PetIndexSerializer < ActiveModel::Serializer
+class PetSerializer < ActiveModel::Serializer
   attributes :id, :name, :intelligence, :happiness, :hunger, :sleepiness, :image, :setting_id
-  has_many :toys, serializer: ToysSerializer
+  has_many :toys, serializer: PetToysSerializer
   belongs_to :setting, serializer: PetSettingSerializer
 end
